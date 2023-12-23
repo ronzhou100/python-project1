6da423ae2954481cc17cdaa410bbb1bc229e2387
Ron Zhou <113530590+ronzhou100@users.noreply.github.com>
2023-12-23 18:20:18 -0500

    Add files via upload

---
+++ name generator.py
@@ -0,0 +1,8 @@
+#create a welcome greeting for your players.
+print("Welcome to the band name generator.")
+#Ask the player the name of the city they grew up in.
+city = input("What is the name of the city you grew up in?\n")
+#Ask the player what their favorite animal is.
+animal = input("What is your favorite animal?\n")
+#combine the name of the city they grew up in with their favorite animal to show them a potential band name. 
+print("Your band name could be The " + city + " " + animal + "s.")
